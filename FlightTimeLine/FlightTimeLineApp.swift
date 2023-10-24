31ba7d4f4778e60bd46ae46359046d740f89c641
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-24 20:15:58 +0300

    Initial Commit

---
+++ FlightTimeLine/FlightTimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimeLineApp.swift
+//  FlightTimeLine
+//
+//  Created by Lera Savchenko on 24.10.23.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
